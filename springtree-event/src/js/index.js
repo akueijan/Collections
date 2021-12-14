64e853be7ec14c97fd0f31a4dab713b708fe2473
akueijan <akueijan@spring-trees.com>
2021-12-14 18:15:27 +0800

    case add

---
+++ springtree-event/src/js/index.js
@@ -0,0 +1,21 @@
+﻿var index_view = new Vue({
+    el: "#app",
+    data: {
+        dataArr: [
+            {
+                name: "周延霖Red"
+            }
+        ]
+    },
+    methods: {
+        kvAni() {
+            let sec = 0.3
+            let tl = gsap.timeline({delay: 0.6})
+                
+        }
+    },
+    mounted: function() {
+        // this.projApi.post(uri, data)  //Ex
+        this.kvAni()
+    }
+})
